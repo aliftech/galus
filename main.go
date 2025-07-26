302d48f29cd05d933dc8b2f881b3d2d830e0f86f
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2025-07-26 16:06:07 +0700

    fix(main.go): update and fix toml config

--- main.go
+++ main.go
@@ -40,9 +40,9 @@ root_dir = "."
 tmp_dir = "tmp"
 include_ext = ["go"]
 exclude_dir = [".git", "vendor", "tmp"]
-build_cmd = "go build -o ./tmp/main ."
-binary_name = "tmp/main"
-command_args = ["tmp/main"]
+build_cmd = "go build -o ./tmp/main.exe ."
+binary_name = "tmp/main.exe"
+command_args = ["tmp/main.exe"]
 `
 
 func main() {
